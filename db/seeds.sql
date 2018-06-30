e7d8019239ef802b60c6306ca02f83298c057dd0
joannelee1990 <33495443+joannelee1990@users.noreply.github.com>
2018-06-30 00:19:33 -0500

    please

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('JoanneBurger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('JennyBurger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('KenBurger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('AlexBurger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('PhilBurger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('MichaelBurger', false);
